7c23ab27d8401e67ed774d9ac0dbfc972f12a061
MaxGolden <maxgold226@gmail.com>
2019-04-07 18:32:48 -0500

    a

--- __init__.py
+++ __init__.py
@@ -77,6 +77,31 @@ class RecipeSkill(MycroftSkill):
         else:
             self.speak_dialog('NotFound')
 
+    @intent_file_handler('food.intent')
+    def get_recipie(self, message):
+        nutrients = search_nutrients(message.data['food'])
+        if nutrients:
+
+            speakNu = nutrients
+            print(speakNu)
+            self.speak_dialog('YouWillNeed', {
+                'ingredients': ', '.join(speakNu[:-1]),
+                'final_ingredient': speakNu[-1]})
+            # self.speak_dialog('okay', {
+            #     'food': ', '.join(nutrients[:-1]),
+            #     'final_food': nutrients[-1]})
+            time.sleep(1)
+
+            self.set_context('NutrientsContext', str(nutrients))
+        else:
+            self.speak_dialog('NotFound')
+
+    @intent_handler(AdaptIntent().require('Nutrients').require('TellMe')
+                                 .require('Again')
+                                 .require('IngredientContext'))
+    def tell_ingredients_again(self, message):
+        return self.repeat_context(message.data['NutrientsContext'])
+
     def repeat_context(self, context):
         self.speak(context)
 
